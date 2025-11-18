e563882c83fc8c5fb1b45dcc29ab6e75ad666234
Roberto Di Lillo <dilillo.roberto@gmail.com>
2025-11-18 16:03:47 -0300

    adds logos

--- app/components/SocialProof/SocialProof.constants.tsx
+++ app/components/SocialProof/SocialProof.constants.tsx
@@ -7,5 +7,9 @@ export const logos = [
   { name: 'Game Padel Center', image: '/images/brand_06.png' },
   { name: 'Heidelberg Materials', image: '/images/brand_07.png' },
   { name: 'Padel Zenter', image: '/images/brand_08.png' },
+  { name: 'Lineapelle', image: '/images/brand_09.png' },
+  { name: 'Radio Deejay', image: '/images/brand_10.png' },
+  { name: 'Amazon', image: '/images/brand_11.png' },
+  { name: 'Audi', image: '/images/brand_12.png' },
 ]
 
